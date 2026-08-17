dd7df5b2888e1f1bb9d924f66ee251992118df50
Ganiu Akownanu <45771378+AkwGaniu@users.noreply.github.com>
2026-08-17 16:51:21 +0100

    updated lambda code

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_function(event, context):
   # TODO implement
   return {
     'statusCode': 200,
-    'body': json.dumps('Hello from Lambda! \n I hope you are good today')
+    'body': json.dumps('Hello Lambda! from VS Code')
   }
